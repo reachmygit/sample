44d2a121d99af140977ecdd1e11e7b8c84d23cc6
Rama <Rama@ITblizInc.attlocal.net>
2018-10-18 09:37:00 -0500

    Test cOMMIT

--- TestProject/src/HelloWord.java
+++ TestProject/src/HelloWord.java
@@ -1,3 +1,7 @@
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloWord {
 	
@@ -5,7 +9,20 @@ public class HelloWord {
 		
 		System.out.println("Hello World");
 		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Rama\\Documents\\Rama\\Java\\Driver\\chromedriver.exe");
 		
-	}
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.com/");
+		//driver.findElement(By.id("username")).sendKeys("hello");
+		//driver.findElement(By.name("pw")).sendKeys("1234");
+		//driver.findElement(By.id("Login")).click();
 
+		//driver.findElement(By.cssSelector("input[id='username']")).sendKeys("hello");
+		//driver.findElement(By.cssSelector("input[name='pw']")).sendKeys("1234");
+		//driver.findElement(By.cssSelector("input[id='Login']")).click();
+
+		//driver.findElement(By.xpath("//div[@class='1st-c']/div/div[2]\div\input"))
+	}
+ 
 }
