f479d5988a16b2837135be8a845c3c34d67d5e6d
Rama <Rama@ITblizInc.attlocal.net>
2018-10-18 09:39:14 -0500

    Test

---
+++ TestProject/src/Frames1Demo.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Frames1Demo {
+	
+	public static void main(String[] a) {
+	
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Rama\\Documents\\Rama\\Java\\Driver\\chromedriver.exe");
+	
+	
+	//mouseover
+	WebDriver driver = new ChromeDriver();
+	}
+
+}
